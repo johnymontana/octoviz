d05179a85422fdc9ab1e4a9403a03f61409b720f
William Lyon <wlyon@quantpost.com>
2013-11-01 19:56:39 -0600

    add default scaffolding

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
-var express = require('express');
+"use strict"
+
+var express = require('express'),
+  hbs = require('hbs');
+
 var app = express();
+app.set('view engine', 'html');
+app.engine('html', require('hbs').__express);
+app.set('views', __dirname + '/views');
+app.use(express.static(__dirname + '/public'));
 app.use(express.logger());
 
 app.get('/', function(req, res){
-  res.send("You dawg");
+  res.render('index_template', {});
 });
 
 var port = process.env.PORT || 5000;
+
+
 app.listen(port, function() {
   console.log("listening on " + port);
 })
