d527df35b20997464de2e395fe570d3d83e35f69
William Lyon <wlyon@quantpost.com>
2013-11-04 23:45:33 -0700

    add all views / data files

--- server.js
+++ server.js
@@ -16,8 +16,31 @@ app.get('/', function(req, res){
 
 app.get('/realtime', function(req, res){
     res.render('cubeism_template', {})
-})
+});
 
+app.get('/own_lang', function(req, res){
+  res.render('force_owned_language', {})
+});
+
+app.get('/own_lang_personal', function(req, res){
+  res.render('force_owned_language_personal', {});
+});
+
+app.get('/star_lang', function(req, res){
+  res.render('force_starred_language', {})
+});
+
+app.get('/star_lang_personal', function(req, res){
+  res.render('force_starred_language_personal', {})
+});
+
+app.get('/star_repo', function(req, res){
+  res.render('force_starred_repos', {});
+});
+
+app.get('/star_repo_personal', function(req, res){
+    res.render('force_starred_repos_personal', {})
+})
 var port = process.env.PORT || 5000;
 
 
